f9f45438dbb1690ef566e3e61427dece5f2e4cf0
Mike Glass <mike.glass@americanreading.com>
2017-09-29 00:15:42 -0400

    setup structure and output hello world.

---
+++ app/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict';
+
+/*
+const program = require('commander');
+
+program
+    .version('0.1.0')
+    .command('')
+    .description('')
+    .option('','')
+    .option('','')
+    .action('');
+
+program.parse(process.argv);*/
+
+console.log('hello world');
